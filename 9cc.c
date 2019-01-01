d5d365bde0421a7c694750edfd38e9e98502d847
nibral <nibral@users.noreply.github.com>
2019-01-01 14:22:47 +0900

    Implement a recursive descendent parser

--- 9cc.c
+++ 9cc.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -58,10 +59,99 @@ void tokenize(char *p) {
     tokens[i].input = p;
 }
 
+// processing token position
+int pos = 0;
+
+// type of node
+enum {
+    ND_NUM = 256    // number
+};
+
+// node
+typedef struct Node {
+    int ty;             // type
+    struct Node *lhs;   // left hand side
+    struct Node *rhs;   // right hand side
+    int val;            // value of ND_NUM
+} Node;
+
+// create new node
+Node *new_node(int op, Node *lhs, Node *rhs) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = op;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// create new number node
+Node *new_node_num(int val) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 // report error
-void error(int i) {
-    fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
     exit(1);
+}
+
+// number parser
+Node *number() {
+    if (tokens[pos].ty == TK_NUM) {
+        return new_node_num(tokens[pos++].val);
+    }
+    error("number expected, but got %s\n", tokens[pos].input);
+}
+
+// expression parser
+Node *expr() {
+    Node *lhs = number();
+    for (;;) {
+        int op = tokens[pos].ty;
+        if (op != '+' && op != '-') {
+            break;
+        }
+        pos++;
+        lhs = new_node(op, lhs, number());
+    }
+
+    if (tokens[pos].ty != TK_EOF) {
+        error("stray token: %s", tokens[pos].input);
+    }
+    return lhs;
+}
+
+// code generator
+char *regs[] = {"rdi", "rsi", "r10", "r11", "r12", "r13", "r14", "r15", NULL};
+int cur;
+
+char *gen(Node *node) {
+    if (node->ty == ND_NUM) {
+        char *reg = regs[cur++];
+        if (!reg) {
+            error("register exhausted");
+        }
+        printf("  mov %s, %d\n", reg, node->val);
+        return reg;
+    }
+
+    char *dst = gen(node->lhs);
+    char *src = gen(node->rhs);
+
+    switch (node->ty) {
+        case '+':
+            printf("  add %s, %s\n", dst, src);
+            return dst;
+        case '-':
+            printf("  sub %s, %s\n", dst, src);
+            return dst;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -70,49 +160,17 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Invalid number of arguments");
     }
 
-    // tokenize
+    // tokenize and parse
     tokenize(argv[1]);
+    Node *node = expr();
 
     // write assembler directives
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // write first move instruction (expect numbers)
-    if (tokens[0].ty != TK_NUM) {
-        error(0);
-    }
-    printf("  mov rax, %d\n", tokens[0].val);
-
-    // write assemblies from tokens like '+ <number>' or '- <number>'
-    int i = 1;
-    while (tokens[i].ty != TK_EOF) {
-        // addition
-        if (tokens[i].ty == '+') {
-            i++;
-            if (tokens[i].ty != TK_NUM) {
-                error(i);
-            }
-            printf("  add rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        // subtraction
-        if (tokens[i].ty == '-') {
-            i++;
-            if (tokens[i].ty != TK_NUM) {
-                error(i);
-            }
-            printf("  sub rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        // show error if unexpected read
-        error(i);
-    }
-
+    // generate assembly code
+    printf("  mov rax, %s\n", gen(node));
     printf("  ret\n");
     return 0;
 }
